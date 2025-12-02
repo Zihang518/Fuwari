ff843adff3378c0d895b569d6e6ddb439cee4b87
二叉树树 <acofork@qq.com>
2025-12-02 18:29:46 +0800

    docs(todo): 更新待办事项列表，添加新视频任务并调整格式
    添加了关于OBS录视频的新任务，同时统一了列表项的缩进格式以提高可读性

--- src/data/todo.ts
+++ src/data/todo.ts
@@ -8,9 +8,10 @@ export const todoConfig: TodoConfig = {
         { content: "做视频：利用STUN在世界各地连接上你的电脑", completed: true  },
         { content: "做视频：Ventoy+FirPE使用（可能不会做）", completed: false },
 		{ content: "做视频：Cloudflare 利用Origin Rules 6转4 访问家里云", completed: false },
+        { content: "做视频：如何使用OBS优雅的录视频", completed: false },
         { content: "做视频：anuneko.com米哈游AI上手体验", completed: true },
         { content: "完善Bot插件：anuneko.com米哈游AI聊天机器人（支持pick）", completed: true },
-{ content: "写文章：当anuneko bot插件完善后，编写开发文章", completed: true },
-{ content: "更新文章：Serverless，添加Render，Zeabur", completed: true },
+        { content: "写文章：当anuneko bot插件完善后，编写开发文章", completed: true },
+        { content: "更新文章：Serverless，添加Render，Zeabur", completed: true },
     ],
 };
