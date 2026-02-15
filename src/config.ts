df1c3803931ca86453e9be365adc72061e7b4f12
XueGao <zihang518@outlook.com>
2026-02-15 10:42:46 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -38,7 +38,7 @@ export const siteConfig: SiteConfig = {
 	},
 	background: {
 		enable: true, // Enable background image
-		src: "", // Background image URL (supports HTTPS)
+		src: "https://p.2x.nz/r/i/h", // Background image URL (supports HTTPS)
 		position: "center", // Background position: 'top', 'center', 'bottom'
 		size: "cover", // Background size: 'cover', 'contain', 'auto'
 		repeat: "no-repeat", // Background repeat: 'no-repeat', 'repeat', 'repeat-x', 'repeat-y'
